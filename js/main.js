5aab7fc45f52da007a38c87acb155e6268464eb7
Pablo Perez <paperezpablo@gmail.com>
2024-12-07 11:46:47 -0300

    changes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const menuBtn = document.getElementById("menu-btn")
 const navLinks = document.getElementById("nav-links")
 const menuBtnIcon = menuBtn.querySelector("i");
 
+// Click events on and off the page
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("open");
 
@@ -11,4 +12,20 @@ menuBtn.addEventListener("click", () => {
 navLinks.addEventListener("click", () => {
     navLinks.classList.remove("open");
     menuBtnIcon.setAttribute("class", "fa-solid fa-bars");
+});
+
+const scrollRevealOption = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+}
+
+// Container header
+ScrollReveal().reveal(".headerContainer p", {
+    ...scrollRevealOption,
+});
+
+ScrollReveal().reveal(".headerContainer h1", {
+    ...scrollRevealOption,
+    delay: 500,
 });
